7a1af71f1c61ff1ca7b520a9c2aead8614647884
NoorSh <noor.shamasneh@relational.ai>
2024-07-18 14:42:42 +0300

    add min and max number of days

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { ZonedDateTime, Duration } = require("@js-joda/core");
+const {ZonedDateTime, Duration} = require("@js-joda/core");
 
 async function run() {
   try {
     const token = core.getInput("token");
     const repository = core.getInput("repository");
-    const numDays = parseInt(core.getInput("days"));
+    let minNumberOfDays = parseInt(core.getInput("minNumberOfDays"));
+    let maxNumberOfDays = parseInt(core.getInput("maxNumberOfDays"));
+
+    // Backward compatibility: if lastUpdatedBefore && lastUpdatedAfter not provided, then use days
+    if (!minNumberOfDays) {
+      minNumberOfDays = parseInt(core.getInput("days"));
+      maxNumberOfDays = 2 * minNumberOfDays;
+    }
+
+
     const octokit = github.getOctokit(token);
     const now = ZonedDateTime.now();
 
@@ -20,11 +29,11 @@ async function run() {
         `Invalid repository '${qualifiedRepository}'. Expected format {owner}/{repo}.`
       );
     }
-    const repo = { owner: splitRepository[0], repo: splitRepository[1] };
+    const repo = {owner: splitRepository[0], repo: splitRepository[1]};
 
     const pulls = await octokit.paginate(
       octokit.rest.pulls.list,
-      { ...repo, state: "open" },
+      {...repo, state: "open"},
       (response) =>
         response.data
           .filter(
@@ -32,7 +41,11 @@ async function run() {
               Duration.between(
                 ZonedDateTime.parse(pr.updated_at),
                 now
-              ).toDays() > numDays
+              ).toDays() > minNumberOfDays &&
+              Duration.between(
+                ZonedDateTime.parse(pr.updated_at),
+                now
+              ).toDays() <= maxNumberOfDays
           )
           .map((pr) => ({
             ref: pr.head.ref,
